83517986a8916060d9b6403302bae3e2e7ec96d7
Anton Popkov <popkov_anton_2007@vk.com>
2024-09-10 17:55:10 +0700

    Reinvent the execution system

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import time
-import msvcrt
-import cutie
 import json
+import cutie
+import msvcrt
 import random
+
 from colorama import Back, Fore
+from keyboard import layout, keys
 
 
 def clear():
-    print('\x1b[2J', end='\r')
+    print('\x1b[2J\x1b[H', end='')
 
 
 amount = 0
@@ -15,6 +17,7 @@ def menu():
     global amount
     amounts = [10, 25, 50, 75, 100]
     clear()
+    print(f'{Fore.BLUE}Typing test{Fore.RESET}\n')
     print('Amount of words:')
     choice = cutie.select(
         amounts,
@@ -27,121 +30,120 @@ def menu():
 
 def gen(amount):
     with open('words.json', 'r') as file:
-        words = json.load(file)
+        list = json.load(file)
 
-    lst = random.sample(words, amount)
-    convert(lst)
+    words = random.sample(list, amount)
+    convert(words)
 
 
 text = ''
-def convert(lst):
-    global text
+indent = 0
+def convert(words):
+    global text, indent
     current = ''
     rest = ''
-    for el in range(1, len(lst)):
-        if el % 10 == 0:
-            lst[el] += '\n'
+    for i, _ in enumerate(words, start=1):
+        if i == len(words):
+            break
+        
+        if i % 10 == 0:
+            words[i-1] += '\n'
         else:
-            lst[el] += ' '
+            words[i-1] += ' '
 
-    result = ''.join(lst[::-1])
-    if len(lst) == 10:
+    result = ''.join(words)
+    if len(words) == 10:
         current = result
+        indent = 1
     else:
-        i = result.rfind('\n')
-        current = result[i+1:]
-        rest = result[:i]
+        sep = result.find('\n')
+        current = result[:sep]
+        rest = result[sep:]
+        indent = len(result.splitlines())
     
     text = result.replace('\n', ' ')
-    output(current, rest)
-
-
-"""
-TODO:
-Redesign the WPM counting system
-"""
+    draw(current, rest)
 
 
 speed = []
 typos = 0
 def stat():
-    global text, speed, typos
+    global speed, typos
     cpm = round((len(text) - typos) / (sum(speed) / 60))
-    wpm = round(len(text.split(' ')) / (sum(speed) / 60))
     accuracy = round((len(text) - typos) / len(text) * 100)
     clear()
-    print(f'\nCPM: {cpm}\nWPM: {wpm}\naccuracy: {accuracy}%\n')
+    print(f'{Fore.BLUE}Statistics:{Fore.RESET}\n')
+    print(f'CPM: {cpm}\naccuracy: {accuracy}%\n')
+    print(f'{Fore.LIGHTBLACK_EX}↱ Ctrl + M    ☓ Ctrl + C{Fore.RESET}')
+    while True:
+        sym = msvcrt.getch()
+        if sym == b'\x03':
+            return
+        
+        if sym == b'\x0D':
+            speed = []
+            typos = 0
+            menu()
+            return
+        
 
-
-keyboard = '\n[ 1 ][ 2 ][ 3 ][ 4 ][ 5 ][ 6 ][ 7 ][ 8 ][ 9 ][ 0 ]\
-            \n[ Q ][ W ][ E ][ R ][ T ][ Y ][ U ][ I ][ O ][ P ]\
-            \n     [ A ][ S ][ D ][ F ][ G ][ H ][ J ][ K ][ L ]\
-            \n     [ Z ][ X ][ C ][ V ][ B ][ N ][ M ]\
-            \n          [                       ]\n'
-keys = {
-    6: {'1': '[ 1 ]', '2': '[ 2 ]', '3': '[ 3 ]', '4': '[ 4 ]', '5': '[ 5 ]', '6': '[ 6 ]', '7': '[ 7 ]', '8': '[ 8 ]', '9': '[ 9 ]', '0': '[ 0 ]'},
-    5: {'q': '[ Q ]', 'w': '[ W ]', 'e': '[ E ]', 'r': '[ R ]', 't': '[ T ]', 'y': '[ Y ]', 'u': '[ U ]', 'i': '[ I ]', 'o': '[ O ]', 'p': '[ P ]'},
-    4: {'a': '     [ A ]', 's': '[ S ]', 'd': '[ D ]', 'f': '[ F ]', 'g': '[ G ]', 'h': '[ H ]', 'j': '[ J ]', 'k': '[ K ]', 'l': '[ L ]'},
-    3: {'z': '     [ Z ]', 'x': '[ X ]', 'c': '[ C ]', 'v': '[ V ]', 'b': '[ B ]', 'n': '[ N ]', 'm': '[ M ]'},
-    2: {' ': '          [                       ]'}
-}
-def output(current, rest=None):
-    global amount, typos, speed
-    str = iter(current)
+def draw(current, rest=None):
+    global typos, speed, indent
+    line = iter(current)
     input = []
     clear()
+    print(current, end='')
     print(Fore.LIGHTBLACK_EX + rest + Fore.RESET)
-    print(current, end='\r')
-    print('\n' + keyboard, end='\r')
+    print(f'\n{layout}')
+    print(f'\n{Fore.LIGHTBLACK_EX}↻ Ctrl + R{Fore.RESET}')
     start = time.time()
 
 
-    def get(key):
-        global typos, keyboard, keys
-        nonlocal input
+    def get(sym):
+        global typos
         try:
-            char = next(str)
-            if key != char:
+            char = next(line)
+            if sym == char:
+                sym = Fore.BLUE + char + Fore.RESET
+            else:
                 typos += 1
-                key = Back.RED + char + Back.RESET
-            else:
-                key = Fore.BLUE + char + Fore.RESET
+                sym = Back.RED + char + Back.RESET
 
-            input.append(key)
+            input.append(sym)
+            print('\x1b[H', end='')
             if len(input) > 1:
-                print(f'\033[7A\033[{len(input)-1}C' + input[-1])
+                print(f'\x1b[{len(input)-1}C' + input[-1])
             else:
-                print(f'\033[7A' + input[-1])
+                print('' + input[-1])
 
-            print(keyboard, end='\r')
         except StopIteration:
             pass
 
-    
+
     while True:
-        key = msvcrt.getch()
-        # CTRL + R
-        if key == b'\x12':
+        sym = msvcrt.getch()
+        if sym == b'\x12':
             speed = []
             typos = 0
-            return gen(amount)
+            gen(amount)
+            return
 
-        key = key.decode()
-        if key.isprintable():
-            get(key)
+        sym = sym.decode()
+        if sym.isprintable():
+            get(sym)
 
         for column, row in keys.items():
-            if key in row:
+            if sym in row:
                 active = ''
-                for _, value in row.items():
-                    if key == _:
+                for key, value in row.items():
+                    if sym == key:
                         active += Fore.BLUE + value + Fore.RESET
                     else:
                         active += value
-
-                print(f'\033[{column}A')
-                print(active, end='\r')
-                print(f'\033[{column-1}B', end='\r')
+                
+                print(f'\x1b[{indent}B{layout}')
+                print(f'\x1b[{column}A{active}')
+                print(f'\x1b[{column-1}B')
 
         if len(input) == len(current):
             break
@@ -150,15 +152,16 @@ def output(current, rest=None):
     if rest != '':
         text = rest.split('\n')
         text = list(filter(None, text))
-        current = text[-1]
+        current = text[0]
         rest = ''
-        for el in text:
-            if el == current:
+        for line in text:
+            if line == current:
                 continue
 
-            rest += f'\n{el}'
+            rest += f'\n{line}'
 
-        output(current, rest)
+        indent = len((current + rest).splitlines())
+        draw(current, rest)
     else:
         stat()
 
